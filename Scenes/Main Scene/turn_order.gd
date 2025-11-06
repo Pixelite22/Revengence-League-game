3283d0b70a2020344f97eb9bd14608ba4e773059
Pixelite22 <114366972+Pixelite22@users.noreply.github.com>
2025-11-06 15:09:25 -0500

    UPDATE 11/6/2025
    I FINALLY GOT THE DYNAMIC TURN SYSTEM WORKING LFG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
    
    Still gotta get moves shmovin and I want to work on a turn timer where icons of each character move across a timeline, but this is a major victory!

--- Scenes/Main Scene/turn_order.gd
+++ Scenes/Main Scene/turn_order.gd
@@ -1,18 +1,30 @@
 extends Control
 
-signal turn_count_continued
+signal turn_count_reached
 
 var global_ctr := 100
 var turn_reached := false
 
 func turn_countdown(fighters: Array): #the array containing the 6 characters on the field.
 	for player in fighters:
-		player.hero_stats.time =- 1
+		player.hero_stats.time -= 1
+		if player.hero_stats.time <= 0:
+			player.hero_stats.time = 0
+		print(player.hero_stats.name, " has ", player.hero_stats.time, " time counters left.")
+	
+	for player in fighters:
 		if player.hero_stats.time <= 0:
 			turn_reached = true
-		return turn_reached
+			print(turn_reached)
+			turn_count_reached.emit(turn_reached)
+			return #turn_reached
+#			break
+
+	await get_tree().create_timer(0.05).timeout #prevents errors and stack overflow
+	turn_countdown(fighters)
 
 func turn_reset(player: Player):
-	player.hero_stats.time = player.hero_stats.max_time
-	turn_reached = false
-	turn_count_continued.emit
+	if player:
+		player.hero_stats.time = player.hero_stats.max_time
+		turn_reached = false
+#		turn_count_reached.emit(false)
